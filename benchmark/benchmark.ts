953f62107f5948310acb0999350adeb30ed14c65
Nisaruj Rattanaaram <nisaruj@hotmail.com>
2020-09-15 22:20:57 +0700

    Add multipleClientBenchmark

--- benchmark/benchmark.ts
+++ benchmark/benchmark.ts
@@ -1,6 +1,6 @@
 import GrpcClient from '../client/grpc_client';
 import RestClient from '../client/rest_client';
-import { Book, BookInterface } from '../client/BookInterface';
+import { BookInterface } from '../client/BookInterface';
 
 function getGrpcClient() {
     return new GrpcClient('127.0.0.1', 50051);
@@ -49,7 +49,30 @@ function concurrentInsert(count: number) {
     }
 }
 
+async function concurrentList(client: BookInterface, count = 1000) {
+    let p = [];
+    for (let i = 0; i < count; i++) {
+        p.push(client.list());
+    }
+    await Promise.all(p);
+}
+
+async function multipleClientBenchmark() {
+    const grpc1 = getGrpcClient();
+    const grpc2 = getGrpcClient();
+    let start = (new Date()).getTime();
+    await Promise.all([largeInsert(grpc1, 500), concurrentList(grpc2, 500)]);
+    console.log('gRPC Response time:', (new Date()).getTime() - start, 'ms');
+
+    const rest1 = getRestClient();
+    const rest2 = getRestClient();
+    start = (new Date()).getTime();
+    await Promise.all([largeInsert(rest1, 500), concurrentList(rest2, 500)]);
+    console.log('Rest Response time:', (new Date()).getTime() - start, 'ms');
+}
+
 
 // benchmark(smallInsert);
 // benchmark(largeInsert);
-benchmark(concurrentInsert(1024));
+// benchmark(concurrentInsert(1024));
+// multipleClientBenchmark();
